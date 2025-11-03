80ce3e8b9dd8fb3cfd16b37d5f8bedbd37895dbe
Derek Ogletree <trenigma@users.noreply.github.com>
2025-11-03 10:29:15 -0800

    bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -17,7 +17,7 @@ terraform {
   }
 
   # Backend configuration - will be initialized with terraform init
-  # Run: terraform init -backend-config="bucket=YOUR_BUCKET_NAME" -backend-config="key=ecs-todo-api/terraform.tfstate"
+  # Run: terraform init -backend-config="bucket=YOUR_BUCKET_NAME" -backend-config="key=ecs-todo-api-project/terraform.tfstate"
   backend "s3" {
     # bucket         = "WILL_BE_PROVIDED_VIA_CLI"
     # key            = "WILL_BE_PROVIDED_VIA_CLI"
